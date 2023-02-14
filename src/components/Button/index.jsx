65e86fb0ac4cbfd9f63b1975764e28e8f7fefde6
Sofia Consolmagno Fontes <sofiaconsolmagno@hotmail.com>
2023-02-14 11:25:48 -0300

    Button component

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,11 +1,12 @@
 import { Container } from "./style";
 
-export function Button({title, loading}){
+export function Button({title, loading = false, ...rest}){
 
   return(
   <Container 
     type="button"
     disabled={loading}
+    {...rest}
   >
     {loading ? 'carregando...' : title }
   </Container>
