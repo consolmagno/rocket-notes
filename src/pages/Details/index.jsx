6ab6ddf0c611c50a6749cc9a423bb666ef5a1a3d
Sofia Consolmagno Fontes <sofiaconsolmagno@hotmail.com>
2023-02-14 17:12:11 -0300

    Add Component: ButtonText

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,12 +3,15 @@ import { Button } from "../../components/Button/index.jsx"
 import { Header } from "../../components/Header/index.jsx"
 import { Section } from "../../components/Section/index.jsx"
 import { Tag } from "../../components/Tag/index.jsx"
+import { ButtonText } from "../../components/ButtonText/index.jsx"
 
 export function Details(){
   return(
     <Container>
       
     <Header/>
+
+    <ButtonText title="Excluir nota"/>
 
     <Section title="Links úteis">
       <Links>
