5d3791ce673e829c8c1274dc96490c6456bf172e
Sofia Consolmagno Fontes <sofiaconsolmagno@hotmail.com>
2023-02-14 16:30:43 -0300

    Add Componente: Section and Tag

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,13 +1,29 @@
-import {Container} from "./style.js"
+import {Container, Links} from "./style.js"
 import { Button } from "../../components/Button/index.jsx"
 import { Header } from "../../components/Header/index.jsx"
+import { Section } from "../../components/Section/index.jsx"
+import { Tag } from "../../components/Tag/index.jsx"
 
 export function Details(){
   return(
     <Container>
+      
     <Header/>
-    
+
+    <Section title="Links úteis">
+      <Links>
+        <li><a href="https://www.rocketseat.com.br/" target={"_blank"}>https://www.rocketseat.com.br/</a></li>
+        <li><a href="https://www.rocketseat.com.br/" target={"_blank"}>https://www.rocketseat.com.br/</a></li>
+      </Links>
+    </Section>
+
+    <Section title="Marcadores">
+      <Tag title="express"/>
+      <Tag title="node js"/>
+    </Section>
+
     <Button title="Voltar"/>
+
     </Container>
   )
 }
