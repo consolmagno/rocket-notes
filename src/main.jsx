70c6045e7a11c9127e13bfd2fc025236a010a1d0
Sofia Consolmagno Fontes <sofiaconsolmagno@hotmail.com>
2023-02-15 17:10:05 -0300

    Add singIn Interface

--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,14 @@ import theme from './styles/theme'
 
 import {Details} from './pages/Details'
 import { Home } from './pages/Home'
+import { SingIn } from './pages/SingIn'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <Home/>
+      <SingIn/>
     </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/SingIn/index.jsx
@@ -0,0 +1,25 @@
+import {FiMail, FiLock} from 'react-icons/fi';
+import {Container, Form, Backgound} from './style';
+import {Input} from '../../components/Input';
+import {Button} from '../../components/Button'
+import {ButtonText} from '../../components/ButtonText';
+
+export function SingIn(){
+  return(
+    <Container>
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+
+        <h2>Faça seu login</h2>
+        <Input placeholder="E-mail" type ="text" icon={FiMail}/>
+        <Input placeholder="Senha" type ="password" icon={FiLock}/>
+
+        <Button title="Entrar"/>
+
+        <a href="#"> Criar Conta</a>
+        </Form>
+        <Backgound/>
+    </Container>
+  );
+}
