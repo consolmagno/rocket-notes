82e36a693f79dbec10fff75123e3604d91ab4772
Sofia Consolmagno Fontes <sofiaconsolmagno@hotmail.com>
2023-02-15 11:44:19 -0300

    Add home interface

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,48 @@
+import {FiPlus, FiSearch} from 'react-icons/fi';
+import {Container, Brand, Menu, Search, Content, NewNote } from './style'
+import {Header} from '../../components/Header'
+import {Input} from '../../components/Input';
+import {ButtonText} from '../../components/ButtonText'
+import {Section} from '../../components/Section'
+import { Note } from '../../components/Note';
+
+export function Home(){
+  return(
+    <Container>
+      <Brand>
+        <h1>Rocketnotes</h1>
+      </Brand>
+      <Header/>
+
+      <Menu>
+        <li><ButtonText title="Todos" isActive/></li>
+        <li><ButtonText title="Frontend"/></li>
+        <li><ButtonText title="Node"/></li>
+      </Menu>
+
+      <Search>
+        <Input placeholder="Pesquisar pelo título" icon={FiSearch}/>
+      </Search>
+
+      <Content>
+        <Section title="Minhas notas">
+          <Note data={{
+            title: 'React Modal', 
+            tags: [
+              {id: '1', name: 'react'},
+              {id: '2', name: 'react'},
+            ]
+          }}
+          />
+          
+        </Section>
+      </Content>
+
+      <NewNote>
+        <FiPlus/>
+        Criar Nota
+      </NewNote>
+      
+    </Container>
+  );
+}
